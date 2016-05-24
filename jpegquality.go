1777f744d2fe9ed93d68093f217608d1cf972302
Liut <liutao@licaigc.com>
2016-05-25 00:37:11 +0800

    add New() with io.SeekReader

--- jpegquality.go
+++ jpegquality.go
@@ -3,35 +3,36 @@ package jpegquality
 import (
 	"bytes"
 	"errors"
-	"fmt"
+	"io"
 	"log"
 	"math"
 )
 
 var (
 	ErrInvalidJPEG = errors.New("Invalid JPEG content")
+	ErrWrongTable  = errors.New("ERROR: Wrong size for quantization table")
 )
 
 type jpegReader struct {
-	br      *bytes.Reader
+	rs      io.ReadSeeker
 	quality int
 }
 
 func NewWithBytes(buf []byte) (jr *jpegReader, err error) {
-	jr = &jpegReader{br: bytes.NewReader(buf)}
+	return New(bytes.NewReader(buf))
+}
+
+func New(rs io.ReadSeeker) (jr *jpegReader, err error) {
+	jr = &jpegReader{rs: rs}
 
 	var (
-		b1, b2 byte
+		sign = make([]byte, 2)
 	)
-	b1, err = jr.br.ReadByte()
+	_, err = jr.rs.Read(sign)
 	if err != nil {
 		return
 	}
-	b2, err = jr.br.ReadByte()
-	if err != nil {
-		return
-	}
-	if b1 != 0xff && b2 != 0xd8 {
+	if sign[0] != 0xff && sign[1] != 0xd8 {
 		err = ErrInvalidJPEG
 	}
 
@@ -40,7 +41,6 @@ func NewWithBytes(buf []byte) (jr *jpegReader, err error) {
 	if err == nil {
 		jr.quality = q
 	}
-
 	return
 }
 
@@ -53,106 +53,75 @@ func (this *jpegReader) readQuality() (q int, err error) {
 		}
 		var (
 			length, index int
-			b1, b2        byte
+			sign          = make([]byte, 2)
 			qualityAvg    = make([]float64, 3)
 		)
-		b1, err = this.br.ReadByte()
-		if err != nil {
-			log.Printf("read err %s", err)
-			return
-		}
-		b2, err = this.br.ReadByte()
+		_, err = this.rs.Read(sign)
 		if err != nil {
 			log.Printf("read err %s", err)
 			return
 		}
 
-		length = int(b1)*256 + int(b2) - 2
+		length = int(sign[0])*256 + int(sign[1]) - 2
 		if length < 0 {
 			length = 0
 		}
-		// log.Printf("length %d from %x %x", length, b1, b2)
 
 		if mark != 0xffdb { // not a quantization table
-			b := make([]byte, length)
-			var rt int
-			rt, err = this.br.Read(b)
+			_, err = this.rs.Seek(int64(length), 1)
 			if err != nil {
-				log.Printf("read err %s", err)
+				log.Printf("seek err %s", err)
 				return
-			} else {
-				log.Printf("read %d bytes", rt)
 			}
 			continue
 		}
 
 		if length%65 != 0 {
 			log.Printf("ERROR: Wrong size for quantization table -- this contains %d bytes (%d bytes short or %d bytes long)\n", length, 65-length%65, length%65)
+			err = ErrWrongTable
+			return
 		}
 
+		log.Printf("length %d", length)
 		log.Print("Quantization table")
 
-		for length > 0 {
-			var precision byte
-			precision, err = this.br.ReadByte()
-			if err != nil {
-				log.Printf("read err %s", err)
-				return
-			}
-			length--
-			index = int(precision) & 0x0f
-			precision = (precision & 0xf0) / 16
+		var tabuf = make([]byte, length)
+		_, err = this.rs.Read(tabuf)
+		if err != nil {
+			log.Printf("read err %s", err)
+			return
+		}
+		for j := 0; j < int(float64(length)/float64(65)); j++ {
+			buf := tabuf[j*65 : (j+1)*65]
+			index = int(buf[0] & 0x0f)
+			precision := (buf[0] & 0xf0) / 16
 			log.Printf("  Precision=%d; Table index=%d (%s)\n", precision, index, getTableName(index))
 
-			var (
-				total, total_num int
-			)
-
-			for length > 0 && total_num < 64 {
-				var b byte
-				b, err = this.br.ReadByte()
-				if err != nil {
-					return
-				}
-
-				if total_num != 0 {
+			var total int
+			for i, b := range buf {
+				if i > 1 { // ignore first value and index
 					total += int(b)
-				} /* ignore first value */
-				length--
-				/* Show quantization table */
-				if total_num%8 == 0 {
-					fmt.Printf("    ")
 				}
-				fmt.Printf("%4d", b)
-				if total_num%8 == 7 {
-					fmt.Printf("\n")
-				}
-				total_num++
 			}
-			total_num--    /* we read 64 bytes, but only care about 63 values */
-			if index < 3 { /* Only track the first 3 quantization tables */
+			log.Printf("total %d", total)
+			qualityAvg[index] = 100.0 - float64(total)/63.0
+		}
 
-				qualityAvg[index] = 100.0 - float64(total)/float64(total_num)
-				fmt.Printf("  Estimated quality level = %5.2f%%\n", qualityAvg[index])
-				if qualityAvg[index] <= 0 {
-					fmt.Printf("  Quality too low; estimate may be incorrect.\n")
-				}
-				/* copy over the Q tables for initialization (in case Cr==Cb) */
-				for i := index + 1; i < 3; i++ {
-					qualityAvg[i] = qualityAvg[index]
-				}
+		if index < 3 {
+			for i := index + 1; i < 3; i++ {
+				qualityAvg[i] = qualityAvg[index]
 			}
+		}
 
-			if index > 0 {
-				// log.Printf("Averages(%d) %.2f %.2f %.2f", index, qualityAvg[0], qualityAvg[1], qualityAvg[2])
-				var diff, qualityF float64
-				diff = math.Abs(qualityAvg[0]-qualityAvg[1]) * 0.49
-				diff += math.Abs(qualityAvg[0]-qualityAvg[2]) * 0.49
-				qualityF = (qualityAvg[0]+qualityAvg[1]+qualityAvg[2])/3.0 + diff
-				q = int(qualityF + 0.5)
-				log.Printf("Average quality: %5.2f%% (%d%%)\n", qualityF, q)
-				return
-			}
+		if index > 0 {
+			log.Printf("Averages(%d) %.2f %.2f %.2f", index, qualityAvg[0], qualityAvg[1], qualityAvg[2])
+			var diff, qualityF float64
+			diff = math.Abs(qualityAvg[0]-qualityAvg[1]) * 0.49
+			diff += math.Abs(qualityAvg[0]-qualityAvg[2]) * 0.49
+			qualityF = (qualityAvg[0]+qualityAvg[1]+qualityAvg[2])/3.0 + diff
+			q = int(qualityF + 0.5)
+			log.Printf("Average quality: %5.2f%% (%d%%)\n", qualityF, q)
+			return
 		}
 	}
 	return
@@ -167,36 +136,21 @@ func getTableName(index int) string {
 
 func (this *jpegReader) readMarker() int {
 	var (
-		b1, b2 byte
-		err    error
+		mark = make([]byte, 2)
+		err  error
 	)
 
-ReadAgainB1:
-	b1, err = this.br.ReadByte()
+ReadAgain:
+	_, err = this.rs.Read(mark)
 	if err != nil {
 		return 0
 	}
-	if b1 != 0xff {
-		b1, err = this.br.ReadByte()
-		if err != nil {
-			return 0
-		}
+	if mark[0] != 0xff || mark[1] == 0xff || mark[1] == 0x00 {
+		goto ReadAgain
 	}
 
-ReadAgainB2:
-	b2, err = this.br.ReadByte()
-	if err != nil {
-		return 0
-	}
-	if b2 == 0xff {
-		goto ReadAgainB2
-	}
-	if b2 == 0x00 {
-		goto ReadAgainB1
-	}
-
-	// log.Printf("get marker %x %x", b1, b2)
-	return int(b1)*256 + int(b2)
+	// log.Printf("get marker %x", mark)
+	return int(mark[0])*256 + int(mark[1])
 }
 
 func (this *jpegReader) Quality() int {
